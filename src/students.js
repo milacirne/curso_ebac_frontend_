5b689d8dcbfb755bdd5335b19c1bdb3078d2e034
Camila Cirne <camila.cirne22@gmail.com>
2023-04-19 17:46:28 +0100

    Exercise Module 25, EBAC Course, Full Stack Developer Python

---
+++ src/students.js
@@ -0,0 +1,22 @@
+class Student {
+    constructor(name, grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+}
+
+const students = [
+    new Student("Sponge Bob", 6),
+    new Student("Son Goku", 3),
+    new Student("Mickey Mouse", 8.5),
+    new Student("Homer Simpson", 4),
+    new Student("Woodpecker", 9.5),
+    new Student("Light Yagami", 10),
+    new Student("Naruto Uzumaki", 5)
+];
+
+function approvedStudents() {
+    return students.filter(student => student.grade >= 6);
+};
+
+console.log(approvedStudents());
