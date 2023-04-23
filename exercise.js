5290f6eb1452b4f994bc7c8ad70cc73cd5d02b3c
Camila Cirne <camila.cirne22@gmail.com>
2023-04-23 23:52:56 +0100

    EBAC Course, TypeScript module, Full Stack Developer

---
+++ exercise.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiply(a, b) {
+    return a * b;
+}
+const result = multiply(2, 5);
+console.log(result);
+function saudation(name) {
+    return `Olá, ${name}! Obrigada por sempre me ajudar!`;
+}
+const resultTwo = saudation("caro tutor");
+console.log(resultTwo);
---
+++ exercise.ts
@@ -0,0 +1,14 @@
+function multiply(a: number, b: number): number {
+    return a * b;
+}
+
+const result = multiply(2, 5);
+console.log(result);
+
+
+function saudation(name: string): string {
+    return `Olá, ${name}! Obrigada por sempre me ajudar!`;
+}
+
+const resultTwo = saudation("caro tutor");
+console.log(resultTwo);
